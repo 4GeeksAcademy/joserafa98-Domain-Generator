0c064eeec120ba62a5a275210d619900ae902ecf
Joserafa98 <JOSERAFAHERNANDEZ.98@GMAIL.COM>
2024-07-12 18:32:12 +0000

    domain generator 1.0

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function DomainGenerator() {
+    let pronoun = ["the", "our", "a", "you"];
+    let adj = ["great", "big", "small", "ugly", "beauty", "first", "last"];
+    let noun = ["jogger", "racoon", "apple", "house", "phone", "kitty"];
+
+    function RandomizetheDomain(array) {
+      let results = Math.floor(Math.random() * array.length);
+      return results;
+    }
+
+    let DomainGenerator =
+      pronoun[RandomizetheDomain(pronoun)] +
+      adj[RandomizetheDomain(adj)] +
+      noun[RandomizetheDomain(noun) + ".com"];
+    return DomainGenerator;
+  }
+  document.querySelector("#botonresultado").addEventListener("click", () => {
+    let resultado = document.querySelector("#domain");
+    resultado.innerHTML = DomainGenerator();
+  });
 };
+// pasos a resolver:
+//1. establecer mis variables
+//2.
