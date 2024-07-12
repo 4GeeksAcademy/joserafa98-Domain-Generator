8844af5b6622420db371df0073db311c8cc4eaa7
Joserafa98 <JOSERAFAHERNANDEZ.98@GMAIL.COM>
2024-07-12 19:07:30 +0000

    fixed

--- src/app.js
+++ src/app.js
@@ -6,27 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  function DomainGenerator() {
-    let pronoun = ["the", "our", "a", "you"];
-    let adj = ["great", "big", "small", "ugly", "beauty", "first", "last"];
-    let noun = ["jogger", "racoon", "apple", "house", "phone", "kitty"];
+  let pronoun = ["the", "our", "a", "you"];
+  let adj = ["great", "big", "small", "ugly", "beauty", "first", "last"];
+  let noun = ["jogger", "racoon", "apple", "house", "phone", "kitty"];
 
-    function RandomizetheDomain(array) {
-      let results = Math.floor(Math.random() * array.length);
-      return results;
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
     }
-
-    let DomainGenerator =
-      pronoun[RandomizetheDomain(pronoun)] +
-      adj[RandomizetheDomain(adj)] +
-      noun[RandomizetheDomain(noun) + ".com"];
-    return DomainGenerator;
   }
-  document.querySelector("#botonresultado").addEventListener("click", () => {
-    let resultado = document.querySelector("#domain");
-    resultado.innerHTML = DomainGenerator();
-  });
 };
+
 // pasos a resolver:
 //1. establecer mis variables
 //2.
