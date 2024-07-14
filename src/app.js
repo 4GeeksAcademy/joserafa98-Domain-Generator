407899483f6f58f9cadbaf5d8d236a7bcf421c74
Joserafa98 <JOSERAFAHERNANDEZ.98@GMAIL.COM>
2024-07-14 09:48:27 +0000

    printing in HTML

--- src/app.js
+++ src/app.js
@@ -6,15 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let pronoun = ["the", "our", "a", "you"];
-  let adj = ["great", "big", "small", "ugly", "beauty", "first", "last"];
-  let noun = ["jogger", "racoon", "apple", "house", "phone", "kitty"];
+  function DomainGenerator() {
+    let pronoun = ["the", "a", "you"];
+    let adj = ["big", "small", "first", "last"];
+    let noun = ["racoon", "house", "phone"];
+    let results = " ";
 
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.length; k++) {
-        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          let domain = pronoun[i] + adj[j] + noun[k] + ".com";
+          console.log(domain);
+          results += domain + "<br>";
+        }
       }
     }
+    return results;
   }
+  document.querySelector("#botonresultado").addEventListener("click", () => {
+    let resultado = document.querySelector("#domain");
+    resultado.innerHTML = DomainGenerator();
+  });
 };
